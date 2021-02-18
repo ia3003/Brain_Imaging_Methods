3a9e546cffc4616a84c3278b89db1f3e1fda1446
ia3003 <78582829+ia3003@users.noreply.github.com>
2021-02-18 17:52:50 +0530

    Update assignment_2.py
    Edited the sequence to display all images and histograms

--- Assignment/assignment_2.py
+++ Assignment/assignment_2.py
@@ -17,32 +17,47 @@ plt.show()
 
 #plot histogram of the image
 hist_original = plt.hist(brain_image, bins=10, align='mid', orientation='vertical', stacked=False)
+#display the histogram for the original image
 plt.show()
 
-#smoothing the image
-brain_image_5=ndimage.gaussian_filter(brain_image, sigma=5)
-brain_image_10=ndimage.gaussian_filter(brain_image, sigma=10)
-brain_image_20=ndimage.gaussian_filter(brain_image, sigma=20)
-brain_image_30=ndimage.gaussian_filter(brain_image, sigma=30)
-brain_image_40=ndimage.gaussian_filter(brain_image, sigma=40)
-brain_image_50=ndimage.gaussian_filter(brain_image, sigma=50)
+#sigma=5
+brain_image_5=ndimage.gaussian_filter(brain_image, sigma=5) #smooth the image with sigma=5 
+plt.imshow(brain_image_5, cmap='Greys_r') #draw the image in greyscale
+plt.show() #display the image
+hist_5=plt.hist(brain_image_5, bins=10, align='mid', orientation='vertical', stacked=False) #plot histogram for the smoothened image
+plt.show() #display the histogram for the smoothened image
 
-#draw the smoothened images
-plt.imshow(brain_image_5, cmap='Greys_r')
-plt.imshow(brain_image_10, cmap='Greys_r')
-plt.imshow(brain_image_20, cmap='Greys_r')
-plt.imshow(brain_image_30, cmap='Greys_r')
-plt.imshow(brain_image_40, cmap='Greys_r')
-plt.imshow(brain_image_50, cmap='Greys_r')
+#sigma=10
+brain_image_10=ndimage.gaussian_filter(brain_image, sigma=10) #smooth the image with sigma=10
+plt.imshow(brain_image_10, cmap='Greys_r') #draw the image in greyscale
+plt.show() #display the image
+hist_10=plt.hist(brain_image_10, bins=10, align='mid', orientation='vertical', stacked=False) #plot histogram for the smoothened image
+plt.show() #display the histogram for the smoothened image
 
-#display the smoothened images
-plt.show()
+#sigma=20
+brain_image_20=ndimage.gaussian_filter(brain_image, sigma=20) #smooth the image with sigma=20
+plt.imshow(brain_image_20, cmap='Greys_r') #draw the image in greyscale
+plt.show() #display the image
+hist_20=plt.hist(brain_image_20, bins=10, align='mid', orientation='vertical', stacked=False) #plot histogram for the smoothened image
+plt.show() #display the histogram for the smoothened image
 
-#plotting histogram for smoothened images
-hist_5=plt.hist(brain_image_5, bins=10, align='mid', orientation='vertical', stacked=False)
-plt.show()
-hist_10=plt.hist(brain_image_10, bins=10, align='mid', orientation='vertical', stacked=False)
-hist_20=plt.hist(brain_image_20, bins=10, align='mid', orientation='vertical', stacked=False)
-hist_30=plt.hist(brain_image_30, bins=10, align='mid', orientation='vertical', stacked=False)
-hist_40=plt.hist(brain_image_40, bins=10, align='mid', orientation='vertical', stacked=False)
-hist_50=plt.hist(brain_image_50, bins=10, align='mid', orientation='vertical', stacked=False)
+#sigma=30
+brain_image_30=ndimage.gaussian_filter(brain_image, sigma=30) #smooth the image with sigma=30
+plt.imshow(brain_image_30, cmap='Greys_r') #draw the image in greyscale
+plt.show() #display the image
+hist_30=plt.hist(brain_image_30, bins=10, align='mid', orientation='vertical', stacked=False) #plot histogram for the smoothened image
+plt.show() #display the histogram for the smoothened image
+
+#sigma=40
+brain_image_40=ndimage.gaussian_filter(brain_image, sigma=40) #smooth the image with sigma=40
+plt.imshow(brain_image_40, cmap='Greys_r') #draw the image in greyscale
+plt.show() #display the image
+hist_40=plt.hist(brain_image_40, bins=10, align='mid', orientation='vertical', stacked=False) #plot histogram for the smoothened image
+plt.show() #display the histogram for the smoothened image
+
+#sigma=50
+brain_image_50=ndimage.gaussian_filter(brain_image, sigma=50) #smooth the image with sigma=50
+plt.imshow(brain_image_50, cmap='Greys_r') #draw the image in greyscale
+plt.show() #display the image
+hist_50=plt.hist(brain_image_50, bins=10, align='mid', orientation='vertical', stacked=False) #plot histogram for the smoothened image
+plt.show() #display the histogram for the smoothened image
